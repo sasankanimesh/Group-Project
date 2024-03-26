2e45338b5e39dafdb3f54a9a88e574d68af76461
sasankanimesh <140873068+sasankanimesh@users.noreply.github.com>
2024-03-26 14:42:27 +0530

    Delete HealthCareApp-LifelinkHub/screens directory

--- HealthCareApp-LifelinkHub/screens/admin-panel/generateSecretKey.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a random string of 64 characters
-const secretKey = crypto.randomBytes(32).toString('hex');
-console.log('Generated secret key:', secretKey);
